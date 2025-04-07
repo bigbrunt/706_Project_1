a8e07d32bcbba126cafb8620c46a68886d840b34
forr815 <109496744+forr815@users.noreply.github.com>
2025-04-07 12:48:12 +1200

    Update TestFilter.ino

--- TestFilter.ino
+++ TestFilter.ino
@@ -1,8 +1,8 @@
 #include "SensorFilter.h"
 
 // Define Kalman filter instance (Q, R, initial estimate, initial error covariance)
-SensorFilter kf(0.5, 5, 0, 1, 3123, -0.9, A7);
-SensorFilter test(0.5, 5, 0, 1, 1, 1, A7);
+SensorFilter kf(0.5, 5, 0, 1, 16705, -1.22, A9);
+SensorFilter test(0.5, 5, 0, 1, 1, 1, A9);
 
 void setup() {
     Serial.begin(115200);
@@ -10,14 +10,14 @@ void setup() {
 
 void loop() {
 
-    int rawValue = analogRead(A7);
+    int rawValue = analogRead(A9);
     int filteredADC = test.read();
-    float distance = 3123 * pow(rawValue, -0.9);
+    float distance = 51926 * pow(rawValue, -1.379);
     // Apply Kalman filter
     float filteredDistance = kf.read();
 
     // Print results
-    Serial.print(filteredADC);
+    Serial.print(rawValue);
         // Print results
     Serial.print("\t");
     Serial.print (distance);
@@ -27,4 +27,3 @@ void loop() {
 
     delay(100); // Adjust based on required update frequency
 }
-
